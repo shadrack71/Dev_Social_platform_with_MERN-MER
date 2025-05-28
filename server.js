ebd44d7d399568dd60359c05774900c37168144e
shadrack <shadrackkinsimba@gmail.com>
2025-05-28 17:36:35 +0300

    add routes

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const connectDB = require('./config/db')
 
 const app = express()
 
+const userRoutes = require('./routes/users')
+const authRoutes = require('./routes/auth')
+const postRoutes = require('./routes/posts')
+const profileRoutes = require('./routes/profile')
+
 // connect database
 connectDB()
 
 app.get('/', (req,res) => res.send('API running'))
+
+// define routes
+app.use('/api/users',userRoutes)
+app.use('/api/auth',authRoutes)
+app.use('/api/profile',profileRoutes)
+app.use('/api/posts',postRoutes)
 
 const PORT = process.env.PORT || 5000
 
