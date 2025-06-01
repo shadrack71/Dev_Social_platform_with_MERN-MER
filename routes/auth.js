5970b1e2572d0cee6066d760a7b79752d6a61322
shadrack <shadrackkinsimba@gmail.com>
2025-06-01 15:32:32 +0300

    implementing login

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,13 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+
+const { query ,check , validationResult } = require('express-validator')
+
 
 // @route GET api/auth 
 // @desc Test route
@@ -16,6 +23,58 @@ router.get('/' , auth , async (req,res) =>{
         console.error(err.message)
         res.status(500).send('server error')
 
+
+    }
+
+})
+// @route POST api/auth 
+// @desc Authenticate user & get token
+// access public
+
+router.post('/' , [
+   
+    check('email','Please include a valid email').isEmail() ,
+    check('password','Password required').exists()
+     
+], async (req,res) =>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    const {  email , password } = req.body
+    try{
+        let user = await User.findOne({email})
+        if(!user){
+           return  res.status(400).json({errors:[{msg:'Invalid credentials'}]})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if(!isMatch){
+            return  res.status(400).json({errors:[{msg:'Invalid credentials'}]})
+         }
+
+        const payload ={
+            user:{
+                id:user.id
+            }
+        }
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn : 360000},
+            (err, token)=>{
+                if(err) throw err
+
+                res.json({ token })
+            }
+        )
+        
+        // res.status(200).send('user registered')
+
+    }catch(err){
+        console.error(err.message)
+        res.status(500).send('Server error')
 
     }
 
