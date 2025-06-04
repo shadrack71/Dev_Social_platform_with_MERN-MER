6bd93e6ff727fb5805783adbafa042f344fd3241
shadrack <shadrackkinsimba@gmail.com>
2025-06-05 02:12:36 +0300

    feat: adding profile user registering

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/User')
 const Profile  = require('../models/Profile')
+const {check , validationResult } = require('express-validator')
 
 // @route GET api/profile/me
 // @desc  Get current users profile
@@ -11,7 +12,7 @@ const Profile  = require('../models/Profile')
 router.get('/me' , auth,  async (req,res) =>{
 
     try{
-        const profile = await Profile.findOne({ user: res.user.id}).populate('user',['name','avatar'])
+        const profile = await Profile.findOne({ user: req.user.id}).populate('user',['name','avatar'])
         if(!profile){
             return res.status(400).json({msg: 'There is no profile for this user'})
 
@@ -23,8 +24,23 @@ router.get('/me' , auth,  async (req,res) =>{
         res.status(500).send('Server Error')
 
     }
-}
-)
+})
+
+// @route POST api/profile
+// @desc  Create or update  user profile
+// access privite
+
+router.post('/', [auth,[
+    check('status','status required').not().isEmpty() ,
+    check('skills','skills required').not().isEmpty()
+]],async(res , req) =>{
+    const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+        const { email , pass} = req.body
+
+})
 
 
 module.exports = router
