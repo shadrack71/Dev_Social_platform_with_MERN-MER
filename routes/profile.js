bd2b1ea515766c50c155178870adc66fe3d8fbb6
shadrack <shadrackkinsimba@gmail.com>
2025-06-25 02:44:34 +0300

    feat:add profile experience

--- routes/profile.js
+++ routes/profile.js
@@ -157,4 +157,55 @@ router.delete('/',auth , async(req,res)=>{
     }
 })
 
+
+// @route PUT api/profile/experience
+// @desc  add  profile experience ,
+// access private
+
+router.put('/experience',[auth,[
+    check('title','title is  required').not().isEmpty() ,
+    check('company','company is required').not().isEmpty(),
+    check('from','from date is  required').not().isEmpty(),
+]], async(req,res)=>{
+    const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+
+        const { 
+            company,
+            title,
+            from,
+            location,
+            to,
+            current,
+            description
+            
+        } = req.body
+
+    const newExp = {
+        title,
+        company,
+        from,
+        location,
+        to,
+        current,
+        description
+    };
+    try {
+        const profile = await Profile.findOne({user:req.user.id})
+        if(!profile) return res.json({msg:'profile not found'})
+        profile.experience.unshift(newExp)
+        await profile.save()
+        res.json(profile)
+
+    } catch (err) {
+
+        console.error(err.message)
+        res.status(500).send('Server error')
+        
+    }
+
+})
+
 module.exports = router
