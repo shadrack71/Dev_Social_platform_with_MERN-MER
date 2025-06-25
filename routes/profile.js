cc9301c4493fc5f035cebfe2ff2ddd7e0e496495
shadrack <shadrackkinsimba@gmail.com>
2025-06-25 03:07:37 +0300

    feat:add & delete  profile education

--- routes/profile.js
+++ routes/profile.js
@@ -232,4 +232,80 @@ router.delete('/experience/:exp_id',auth , async(req,res)=>{
     }
 })
 
+
+
+// @route PUT api/profile/education
+// @desc  add  profile education ,
+// access private
+
+router.put('/education',[auth,[
+    check('school','school is  required').not().isEmpty() ,
+    check('degree','degree is required').not().isEmpty(),
+    check('fieldofstudy','fieldofstudy is required').not().isEmpty(),
+    check('from','from date is  required').not().isEmpty(),
+]], async(req,res)=>{
+    const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+
+        const { 
+            school,
+            degree,
+            from,
+            fieldofstudy,
+            to,
+            current,
+            description
+            
+        } = req.body
+
+    const newEdu = {
+        school,
+        degree,
+        from,
+        fieldofstudy,
+        to,
+        current,
+        description
+    };
+    try {
+        const profile = await Profile.findOne({user:req.user.id})
+        if(!profile) return res.json({msg:'profile not found'})
+        profile.education.unshift(newEdu)
+        await profile.save()
+        res.json(profile)
+
+    } catch (err) {
+
+        console.error(err.message)
+        res.status(500).send('Server error')
+        
+    }
+
+})
+
+// @route DELETE api/education/edu_id
+// @desc  DELETE  education
+// access private
+
+router.delete('/education/:exp_id',auth , async(req,res)=>{
+    try {
+        //remove bexperience 
+        
+        const profile = await Profile.findOne({user:req.user.id})
+        const removeIndex = profile.education.map(item =>item.id).indexOf(req.params.edu_id)
+        profile.experience.splice(removeIndex,1)
+        await profile.save()
+        res.json(profile)
+        
+    } catch (err) {
+        console.error(err.message)
+        if(err.kind == 'ObjectId'){
+            res.status(400).json({msg:' no profile found'})
+        }
+        res.status(500).send('Server error')
+    }
+})
+
 module.exports = router
