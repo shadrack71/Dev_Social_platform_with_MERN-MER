aef38d70a442e92dfbac886b8f2435c56c9e3179
shadrack <shadrackkinsimba@gmail.com>
2025-07-27 01:57:19 +0300

    feat:register form  and useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter as Router,Route,Routes,Switch} from 'react-router-dom'
+import { BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import Navbar from  './components/layout/Navbar'
 import Landing from  './components/layout/Landing'
 import Login from  './components/auth/Login'
