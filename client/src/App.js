82169d893c7fae7257b7f3ddfb5fdf5fd38ac9f6
shadrack <shadrackkinsimba@gmail.com>
2025-07-27 01:25:17 +0300

    feat:setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 
 import './App.css';
+import { BrowserRouter as Router,Route,Routes,Switch} from 'react-router-dom'
 import Navbar from  './components/layout/Navbar'
 import Landing from  './components/layout/Landing'
+import Login from  './components/auth/Login'
+import Register from  './components/auth/Register'
 
 const  App =() => {
   return (
-    <div >
+    <>
+    <Router>
       <Navbar/>
-      <Landing/>
+      <Routes>
+        <Route exact path='/' Component={Landing}/>
+      </Routes>
+      <section className='container'>
+        <Routes>
+        
+          <Route exact path='/register' Component={Register}/>
+          <Route exact path='/login' Component={Login}/>
+        
+        </Routes>
+
+      </section>
       
-    </div>
+    </Router>
+    </>
   );
 }
 
