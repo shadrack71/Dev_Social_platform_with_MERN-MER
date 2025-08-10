ff7834a36ed5c0ad315ca6835430401546de851d
shadrack <shadrackkinsimba@gmail.com>
2025-08-10 16:36:01 +0300

    feat:login and  signup  UI

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React ,{Fragment , useState}from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
   const [formData , setFormData]= useState({
@@ -8,13 +9,34 @@ const Register = () => {
     password:'',
     password2:''
   })
-  const onSubmit = e =>{
+  const onSubmit =  async e =>{
     e.preventDefault()
     if(password !== password2){
       console.log('Password do not match')
 
     }else{
-      console.log(formData)
+      const newUser = {
+        name,
+        email,
+        password
+      }
+      try {
+        const config = {
+          headers:{
+            'Content-type':'application/json'
+          }
+        }
+        const body = JSON.stringify(newUser)
+        const res = await axios.post('/api/users',body, config)
+        console.log(res.data)
+        
+      } catch (err) {
+        console.error(err.response.data)
+        
+        
+      }
+      
+
     }
   }
   const {name , email,password,password2} = formData
